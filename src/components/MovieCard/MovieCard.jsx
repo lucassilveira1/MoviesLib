c96ece41af85538474cc8856491c855ff121def7
Lucas <lucas.silveira0516@gmail.com>
2024-09-30 01:31:05 -0300

    fix:delete import line

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
-import "./movieCard.css";
 
 const imageUrl = import.meta.env.VITE_IMG;
 
