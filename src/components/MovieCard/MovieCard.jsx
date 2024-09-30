bd986ad401ca7db4abad01487b711612a6644f0a
Lucas <lucas.silveira0516@gmail.com>
2024-09-30 01:29:39 -0300

    feat:responsive-css

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable no-unused-vars */
-import { Link } from "react-router-dom"
-import { FaStar } from "react-icons/fa"
+import { Link } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
+import "./movieCard.css";
 
-const imageUrl = import.meta.env.VITE_IMG
+const imageUrl = import.meta.env.VITE_IMG;
 
 const MovieCard = ({ movie, showLink = true }) => {
-  return (
-    <div className="movie-card">
-        <img src={imageUrl + movie.poster_path} alt={movie.title} />
-        <h2>{movie.title}</h2>
-        <p>
-            <FaStar /> {movie.vote_average}
-        </p>
+    return (
+        <div className="movie-card">
+            <img src={imageUrl + movie.poster_path} alt={movie.title} />
+            <h2>{movie.title}</h2>
+            <p>
+                <FaStar /> {movie.vote_average}
+            </p>
 
-        {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
-    </div>
-  )
-}
+            {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+        </div>
+    );
+};
 
-export default MovieCard
+export default MovieCard;
